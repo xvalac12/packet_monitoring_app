04a56dc0c2f795a723a0981ab73d62a24ac2e52d
Martin Valach <xvalac12@stud.fit.vutbr.cz>
2024-11-07 14:15:19 +0100

    feat: :sparkles: Basic interface opening
    Added interface opening on constant name of interface.
    
    Opening of interface is working

--- src/isa-top.cpp
+++ src/isa-top.cpp
@@ -1,10 +1,31 @@
+#include <iostream>
 #include <stdio.h>
 #include <pcap.h>
+#include <string>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-	char *dev = argv[1];
+    // enp4s0
+    char errbuf[PCAP_ERRBUF_SIZE];
 
-	printf("Device: %s\n", dev);
-	return(0);
+	const string device = "enp4s0";
+    pcap_t *handler;
+
+    // cout << device << endl;
+
+    // 1. say device we want to sniff
+    // 2. make ruleset (filter), which protocols, ports, etc. (string)
+    // 3. enter sniffing loop
+
+
+    // device name, maximum number of bytes to capture, promisc. mode, timeout in ms, string for error
+    handler = pcap_open_live(device.c_str(), BUFSIZ, true, 1000, errbuf);
+	if (handler == NULL)
+    {
+        cerr << "Error in opening device " << errbuf << endl; 
+    }
+    
+
 }
