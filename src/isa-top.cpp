df4448eafbce8ed51308c4d309e417118fa9b86b
Martin Valach <xvalac12@stud.fit.vutbr.cz>
2024-11-12 16:29:33 +0100

    feat: :sparkles: Basic packet capturing
    Application now captures packets with filter set for only UDP, TCP and ICMP protocols

--- src/isa-top.cpp
+++ src/isa-top.cpp
@@ -3,7 +3,14 @@
 #include <pcap.h>
 #include <string>
 
+
 using namespace std;
+
+void callback(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+    return;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -11,14 +18,20 @@ int main(int argc, char *argv[])
     char errbuf[PCAP_ERRBUF_SIZE];
 
 	const string device = "enp4s0";
+    const string ruleset = "udp or tcp or icmp";
+    struct bpf_program filter;
+    auto filter_err = 0;
     pcap_t *handler;
 
-    // cout << device << endl;
+    bpf_u_int32 mask;
+    bpf_u_int32 address;
 
-    // 1. say device we want to sniff
-    // 2. make ruleset (filter), which protocols, ports, etc. (string)
-    // 3. enter sniffing loop
 
+    if (pcap_lookupnet(device.c_str(), &address, &mask, errbuf) == -1)
+    {
+        cerr << "No netmask for interface" << endl;
+        address = 0, mask = 0;
+    }
 
     // device name, maximum number of bytes to capture, promisc. mode, timeout in ms, string for error
     handler = pcap_open_live(device.c_str(), BUFSIZ, true, 1000, errbuf);
@@ -26,6 +39,19 @@ int main(int argc, char *argv[])
     {
         cerr << "Error in opening device " << errbuf << endl; 
     }
-    
+    // handler, struct of filter, string with rules, is optimized, address of interface
+    filter_err = pcap_compile(handler, &filter, ruleset.c_str(), 0, address);
+    // handler, struct of filter
+    filter_err = pcap_setfilter(handler, &filter);
 
+    if (filter_err == -1)
+    {
+        cerr << "Error in setting filter: "<< pcap_geterr(handler) << endl;
+        return(-2);
+    }
+    
+    pcap_loop(handler, 1, callback, NULL);
+    
+	pcap_freecode(&filter);
+    pcap_close(handler);
 }
